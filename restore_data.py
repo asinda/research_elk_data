8772b3f44d852222b52bfb65b3e3dde3d2ba4065
ALICE SINDAYIGAYA <alicesindayigaya@gmail.com>
2023-07-03 17:21:57 +0100

    Update restore_data.py

--- restore_data.py
+++ restore_data.py
@@ -1,118 +1,84 @@
 # -*- coding: utf-8 -*-
+
 import os
 import json
 import requests
 import base64
-from datetime import datetime, timedelta 
+from datetime import datetime, timedelta
 
 
-def extract_data(start_date, end_date):
-    base_url = "http://localhost:9200"  # URL de votre cluster Elasticsearch
+def get_data_from_elasticsearch(start_date, end_date, from_, size):
+    base_url = "https://it-log-nadar.z3.r03.local:9200/restored-logstash-it-security-*/_search"
     
-    index_name = 'logstash-p-test-*'
-
+    print(base_url)
     # Authentication credentials
     username = 'elastic'
-    password = 'changeme'
+    password = 'w0RKbzBpz!lLXUTev4awBaOEUENmERhL'
 
     # Base64 encode the username and password
     credentials = base64.b64encode('{}:{}'.format(username, password).encode('utf-8')).decode('utf-8')
+    headers = {
+        'Authorization': 'Basic {}'.format(credentials),
+        'Content-Type': 'application/json'
+               }
+    query = {
 
-    # Configuration des paramètres de pagination
-    page_size = 3
-    from_index = 0
-    total_hits = 1
-    # Function to store documents in a JSON file
-    def store_documents(documents, folder, file_name):
-        try:
-            file_path = os.path.join(folder, file_name)
-            with open(file_path, 'w') as file:
-                json.dump(documents, file)
-            print("Data exported to '{}'".format(file_path))
-        except IOError as e:
-            print("Error storing data in file: {}".format(e))
-            raise
-    # Boucle pour itérer sur les résultats paginés
-    while from_index < total_hits:
-        try:
-            # Construction de l'URL de la requête de recherche
-            url = "{}/{}/_search".format(base_url, index_name)
-            headers = {
-                'Authorization': 'Basic {}'.format(credentials),
-                'Content-Type': 'application/json'
-             }
-
-            # Construction de la requête de recherche
-            query = {
-                "query": {
-                    "range": {
-                        "timestamp": {
-                            "gte": start_date.strftime('%Y-%m-%d'),
-                            "lt": (start_date + timedelta(days=1)).strftime('%Y-%m-%d')
-                        }
-                    }
-                },
-                "from": from_index,
-                "size": page_size
+        "query": {
+          "bool": {
+            "must": [
+              {
+               "range": {
+                "@timestamp": {
+                    "gte": "2023-02-28",
+                    "lt": "2023-03-22"
+                }
+               }
+            },
+           {
+            "match": {
+                "message": "marechac"
             }
+             
+          }
+            ]
+          }
+        },
+        "from": from_,
+        "size": size
+    }
   
-            total_documents = 0
-            current_documents = []
+    try:
+        response = requests.get(base_url, headers=headers, json=query, verify=False)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print("Error during the API request:", e)
+        return None
 
-            # Envoi de la requête de recherche à Elasticsearch
-            response = requests.get(url,headers=headers, json=query)
-            response.raise_for_status()
+def main(start_date, end_date, batch_size=5000):
+    from_ = 0
+    total_documents = batch_size
+    filename2 = ""
+    while batch_size >= total_documents: 
+        data = get_data_from_elasticsearch(start_date, end_date, from_, batch_size)
+        total_documents = len(data['hits']['hits'])
+        for hit in data['hits']['hits']:   
+            timestamp = hit['_source']['@timestamp']
+            timedata = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
+            timestamp_data= timedata.strftime('%Y-%m-%d')
+            filename = '/extract/data_{}.json'.format(timestamp_data)         
+            if filename != filename2 :
+                # if  f :
+                #     f.close()
+                f = open(filename, "w")
+                        
+            json.dumps(hit['_source'], f)      
+            filename2 = filename
 
-            # Extraction des résultats
-            data = response.json()["hits"]["hits"]
-
-            # Vérification du nombre total de résultats
-            if from_index == 0:
-                total_hits = response.json()["hits"]["total"]["value"]
-
-            # Traitement des résultats
-            for item in data:
-                # Extraire les données dont vous avez besoin
-                # (exemple : `item["_source"]["field"]`)
-
-                # Stockage des données par date dans un dossier
-              
-                current_date = start_date
-                while current_date <= end_date:
-                    date = current_date.strftime('%Y-%m-%d')
-                    folder_path = os.path.join("/home/alice/Documents/Alice/004-Elk/research_elk_data/data/", date)
-                    if not os.path.exists(folder_path):
-                        os.makedirs(folder_path)
-                    # Écriture des données dans un fichier JSON
-                    current_documents.append(item['_source'])
-                    total_documents += 1
-                    
-                    if len(current_documents) >= page_size:
-                        file_name = "data_{}_{}.json".format(date, total_documents)
-                        store_documents(current_documents, folder_path, file_name)
-                        current_documents = []
-                # Store remaining documents in a final file
-                if current_documents:
-                  file_name = "data_{}_{}.json".format(current_date.strftime('%Y-%m-%d'), total_documents)
-                  store_documents(current_documents, folder_path, file_name)
-
-                current_date += timedelta(days=1)
-                # Mise à jour de l'index de pagination
-                from_index += page_size
-        except requests.exceptions.RequestException as e:
-            print("Erreur lors de la requête : {}".format(str(e)))
-            break
-
-        except KeyError as e:
-            print("Clé introuvable dans la réponse JSON : {}".format(str(e)))
-            break
-
-        except IOError as e:
-            print("Erreur d'entrée/sortie lors de l'écriture du fichier : {}".format(str(e)))
-            break
+            from_ += batch_size
 
 
-# Define range date for the request
-start_date = datetime(2023, 6, 27)
-end_date = datetime(2023, 7, 7)
-extract_data(start_date, end_date)
+if __name__ == "__main__":
+    start_date = datetime(2023, 2, 27)
+    end_date = datetime(2023, 3, 22)
+    main(start_date, end_date)

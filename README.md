ffd596df9637c84d47f8db1e54e5d6e1cecf5d7a
alicesindayigaya <alicesindayigaya@gmail.com>
2023-07-04 05:43:46 +0200

    update the script

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # research_elk_data
+Elasticsearch Data Retrieval and Storage
+
+This Python script allows you to retrieve data from an Elasticsearch index and store the documents in separate JSON files based on their timestamps.
+Prerequisites
+
+Before running the script, ensure you have the following prerequisites:
+
+    Python 3.x installed on your machine
+    Required Python packages: requests
+
+Install the required package using pip:
+
+pip install requests
+
+Configuration
+
+In the script, you may need to update the Elasticsearch URL and authentication credentials to match your Elasticsearch instance. Edit the following lines in the get_data_from_elasticsearch function:
+
+python
+
+base_url = "http://localhost:9200/logstash-p-test-*/_search"
+username = 'elastic'
+password = 'changeme'
+
+Adjust the base_url to the appropriate Elasticsearch URL for your specific index and cluster.
+Usage
+
+To run the script, use the following command:
+
+bash
+
+python script_name.py
+
+Replace script_name.py with the actual filename of the Python script.
+Parameters
+
+The main function main() takes three optional parameters:
+
+    start_date: A datetime object representing the start date of the data retrieval period.
+    end_date: A datetime object representing the end date of the data retrieval period.
+    batch_size: The number of documents to retrieve from Elasticsearch in each API request.
+
+By default, start_date and end_date are set to datetime(2023, 6, 27) and datetime(2023, 7, 22), respectively. The batch_size is set to 40 documents.
+Output
+
+The script will retrieve data from the specified Elasticsearch index using the given time range and store each document in a separate JSON file. The files will be named based on their timestamps, following the format: data_<timestamp>.json.
+
+If a document with the same timestamp has already been retrieved and stored in a file, it will be skipped to avoid duplication.
+
+Note: If there are any issues during data retrieval or file writing, the script will print error messages to the console.
+
+Please make sure you have proper access rights to read from the Elasticsearch index and write files to the local directory where the script is running.
--- restore_data.py
+++ restore_data.py
@@ -8,77 +8,106 @@ from datetime import datetime, timedelta
 
 
 def get_data_from_elasticsearch(start_date, end_date, from_, size):
-    base_url = "https://it-log-nadar.z3.r03.local:9200/restored-logstash-it-security-*/_search"
+    base_url = "http://localhost:9200/logstash-p-test-*/_search"
     
+    # Print base URL for debugging
     print(base_url)
+    
     # Authentication credentials
     username = 'elastic'
-    password = 'w0RKbzBpz!lLXUTev4awBaOEUENmERhL'
+    password = 'changeme'
 
     # Base64 encode the username and password
     credentials = base64.b64encode('{}:{}'.format(username, password).encode('utf-8')).decode('utf-8')
     headers = {
         'Authorization': 'Basic {}'.format(credentials),
         'Content-Type': 'application/json'
-               }
+    }
+    
+    # Build the Elasticsearch query
     query = {
-
         "query": {
-          "bool": {
-            "must": [
-              {
-               "range": {
-                "@timestamp": {
-                    "gte": "2023-02-28",
-                    "lt": "2023-03-22"
-                }
-               }
-            },
-           {
-            "match": {
-                "message": "marechac"
+            "bool": {
+                "must": [
+                    {
+                        "range": {
+                            "timestamp": {
+                                "gte": start_date.strftime('%Y-%m-%d'),
+                                "lt": end_date.strftime('%Y-%m-%d')
+                            }
+                        }
+                    },
+                    {
+                        "match": {
+                            "content": "example"
+                        }
+                    }
+                ]
             }
-             
-          }
-            ]
-          }
         },
         "from": from_,
         "size": size
     }
   
     try:
+        # Send the HTTP request to Elasticsearch
         response = requests.get(base_url, headers=headers, json=query, verify=False)
-        response.raise_for_status()
+        response.raise_for_status()  # Raise an exception if the response status is not successful
         return response.json()
     except requests.exceptions.RequestException as e:
+        # Handle request exceptions
         print("Error during the API request:", e)
         return None
 
-def main(start_date, end_date, batch_size=5000):
+def main(start_date, end_date, batch_size=40):
     from_ = 0
     total_documents = batch_size
     filename2 = ""
-    while batch_size >= total_documents: 
+    existing_documents = set()
+    
+    while from_ < total_documents: 
         data = get_data_from_elasticsearch(start_date, end_date, from_, batch_size)
-        total_documents = len(data['hits']['hits'])
+        
+        if data is None:
+            # Handle the case when the API request fails
+            print("Failed to retrieve data from Elasticsearch.")
+            break
+        
+        total_documents = data['hits']['total']['value']
+        print(total_documents)
+        
         for hit in data['hits']['hits']:   
-            timestamp = hit['_source']['@timestamp']
-            timedata = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
-            timestamp_data= timedata.strftime('%Y-%m-%d')
-            filename = '/extract/data_{}.json'.format(timestamp_data)         
-            if filename != filename2 :
-                # if  f :
-                #     f.close()
-                f = open(filename, "w")
-                        
-            json.dumps(hit['_source'], f)      
-            filename2 = filename
+            timestamp = hit['_source']['timestamp']
+            timedata = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S")
+            timestamp_data = timedata.strftime('%Y-%m-%d')
+            filename = 'data_{}.json'.format(timestamp_data)         
 
-            from_ += batch_size
+            if filename != filename2:
+                if 'f' in locals():
+                    f.close()
+                f = open(filename, "a")
+
+            document_id = hit['_id']
+            if document_id not in existing_documents:
+                try:
+                    json.dump(hit['_source'], f) 
+                    f.write("\n")
+                    existing_documents.add(document_id)
+                    print(existing_documents)
+                except Exception as e:
+                    # Handle the case when writing to the file fails
+                    print("Failed to write data to file:", e)
+            
+            filename2 = filename
+        
+        from_ += batch_size
+        print(from_)
+    
+    if 'f' in locals():
+        f.close()
 
 
 if __name__ == "__main__":
-    start_date = datetime(2023, 2, 27)
-    end_date = datetime(2023, 3, 22)
+    start_date = datetime(2023, 6, 27)
+    end_date = datetime(2023, 7, 22)
     main(start_date, end_date)
